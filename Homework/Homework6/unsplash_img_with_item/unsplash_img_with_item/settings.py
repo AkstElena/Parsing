fed3c07be2fab2f7ff86127a2bddbc4e61e3950a
Akst Elena <akstelena@yandex.ru>
2024-07-25 22:22:47 +0300

    homework6_correct

--- Homework/Homework6/unsplash_img_with_item/unsplash_img_with_item/settings.py
+++ Homework/Homework6/unsplash_img_with_item/unsplash_img_with_item/settings.py
@@ -26,8 +26,8 @@ ROBOTSTXT_OBEY = True
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
 #DOWNLOAD_DELAY = 3
-# DOWNLOAD_DELAY = 3
-# RANDOMIZE_DOWNLOAD_DELAY = True
+DOWNLOAD_DELAY = 3
+RANDOMIZE_DOWNLOAD_DELAY = True
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
